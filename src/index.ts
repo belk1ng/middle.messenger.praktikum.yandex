98650892d40c9ea6a3718f132e64a6c589f0ad73
Dmitry Belkin <dmitriy.belkin.2001@mail.ru>
2023-03-06 20:31:10 +0300

    refactor: 💡 clean logs

--- src/index.ts
+++ src/index.ts
@@ -16,21 +16,15 @@ if (!root) {
 const currentPath = window.location.pathname;
 
 if (currentPath === routes.login) {
-  console.log("LoginPage content");
   root.innerHTML = LoginPage;
 } else if (currentPath === routes.register) {
-  console.log("RegisterPage content");
   root.innerHTML = RegisterPage;
 } else if (currentPath === routes.profile) {
-  console.log("ProfilePage content");
   root.innerHTML = ProfilePage;
 } else if (currentPath === routes.chats) {
-  console.log("ChatsPage content");
   root.innerHTML = ChatsPage;
 } else if (currentPath === routes.error) {
-  console.log("500 Page content");
   root.innerHTML = ServerErrorPage;
 } else {
-  console.log("404 Page content");
   root.innerHTML = NotFoundPage;
 }
