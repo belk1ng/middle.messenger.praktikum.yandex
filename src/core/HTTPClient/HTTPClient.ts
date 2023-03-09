c6d39ba0486684a05445871d0e8a04c8362e76d1
Dmitry Belkin <dmitriy.belkin.2001@mail.ru>
2023-03-09 21:53:19 +0300

    refactor: 💡 HTTPClient
    refactor HTTPClient types & it's onload callback. Check xhr status for
    resolve or reject it's response

---
+++ src/core/HTTPClient/HTTPClient.ts
@@ -0,0 +1,107 @@
+import {
+  HTTP_METHODS,
+  HTTPClient,
+  HTTPClientOptions,
+  HTTPClientForRequest,
+} from "./types";
+
+class HTTPTransport<T> implements HTTPClient<T> {
+  private queryStringify = (data: Record<string, unknown>) =>
+    "?" +
+    Object.entries(data)
+      .map(([key, value]) => `${key}=${value}`)
+      .join("&");
+
+  public get = (url: string, options?: HTTPClientOptions): Promise<T | Error> =>
+    this._request(
+      url,
+      { ...options, method: HTTP_METHODS.GET },
+      options?.timeout
+    );
+
+  public put = (url: string, options: HTTPClientOptions): Promise<T | Error> =>
+    this._request(
+      url,
+      { ...options, method: HTTP_METHODS.PUT },
+      options.timeout
+    );
+
+  public patch = (
+    url: string,
+    options: HTTPClientOptions
+  ): Promise<T | Error> =>
+    this._request(
+      url,
+      { ...options, method: HTTP_METHODS.PATCH },
+      options.timeout
+    );
+
+  public post = (url: string, options: HTTPClientOptions): Promise<T | Error> =>
+    this._request(
+      url,
+      { ...options, method: HTTP_METHODS.POST },
+      options.timeout
+    );
+
+  public delete = (
+    url: string,
+    options?: HTTPClientOptions
+  ): Promise<T | Error> =>
+    this._request(
+      url,
+      { ...options, method: HTTP_METHODS.DELETE },
+      options?.timeout
+    );
+
+  private _request = (
+    url: string,
+    options: HTTPClientForRequest,
+    timeout = 5000
+  ): Promise<T | Error> => {
+    const { data, method, headers = {} } = options;
+
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+
+      const isGetRequest = method === HTTP_METHODS.GET;
+
+      const _url =
+        isGetRequest && data ? `${url}${this.queryStringify(data)}` : url;
+
+      xhr.open(method, _url);
+      xhr.timeout = timeout;
+
+      xhr.onload = () => {
+        console.log(xhr);
+
+        // Only resolve 1**, 2** and 3** response codes
+        if (xhr.status < 400) {
+          resolve(JSON.parse(xhr.response));
+        } else {
+          reject(new Error("Error while request"));
+        }
+      };
+
+      // Reject _request
+      xhr.onabort = () => reject(new Error("Aborted"));
+      xhr.onerror = () => reject(new Error("Request error"));
+      xhr.ontimeout = () => reject(new Error("Timeout end"));
+
+      // Set headers
+      const headersEntries = Object.entries(headers);
+      if (headersEntries.length > 0) {
+        headersEntries.forEach(([header, value]) =>
+          xhr.setRequestHeader(header, value)
+        );
+      }
+
+      if (isGetRequest || !data) {
+        xhr.send();
+      } else {
+        xhr.send(JSON.stringify(data));
+      }
+    });
+  };
+}
+
+export default HTTPTransport;
---
+++ src/core/HTTPClient/types.ts
@@ -0,0 +1,25 @@
+export enum HTTP_METHODS {
+  GET = "GET",
+  POST = "POST",
+  PUT = "PUT",
+  PATCH = "PATCH",
+  DELETE = "DELETE",
+}
+
+export interface HTTPClientOptions {
+  data?: Record<string, unknown>;
+  headers?: Record<string, string>;
+  timeout?: number;
+}
+
+export type HTTPClientForRequest = Omit<HTTPClientOptions, "timeout"> & {
+  method: HTTP_METHODS;
+};
+
+export interface HTTPClient<T> {
+  get: (url: string, options?: HTTPClientOptions) => Promise<T | Error>;
+  put: (url: string, options: HTTPClientOptions) => Promise<T | Error>;
+  patch: (url: string, options: HTTPClientOptions) => Promise<T | Error>;
+  post: (url: string, options: HTTPClientOptions) => Promise<T | Error>;
+  delete: (url: string, options?: HTTPClientOptions) => Promise<T | Error>;
+}
